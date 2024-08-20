bd5c0e815762c3e02822f186e4320023a9269658
ajayrm12 <116661748+ajayrm12@users.noreply.github.com>
2024-08-20 17:45:42 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -37,24 +37,24 @@ root.config(bg="#6F8FAF")
   
 
 
-# Main Frame
+
 Main_frame = LabelFrame(root , padx=100 ,  pady=7 , borderwidth=3 ,  relief="raised")
 Main_frame.config(bg="#6F8FAF")
 Main_frame.grid(row = 0 ,  column= 1 ,  padx= 55 ,  pady =  10)
 
-# Text Lable 
+
 Text_lable = Label(Main_frame, text = "AI Assistant" , font=("comic Sans ms" ,  14 , "bold" ) , bg = "#356696")
 Text_lable.grid(row=0 ,  column=0 , padx=20 , pady= 10)
 
 
-# Image 
+
 Display_Image = ImageTk.PhotoImage(Image.open("image/assitant.png"))
 Image_Lable = Label(Main_frame, image= Display_Image)
 Image_Lable.grid(row = 1 ,  column=0 , pady=20)
 
 
 
-# Add a text widget
+
 
 text=Text(root , font= ('Courier 10 bold') , bg = "#356696")
 text.grid(row = 2,  column= 0)
@@ -63,21 +63,21 @@ text.place(x= 100, y= 375, width= 375, height= 100)
 
 
 
-# Add a entry widget
+
 entry1 = Entry(root, justify = CENTER)
 entry1.place(x=100 , y = 500 , width= 350, height= 30)
 
 
 
-# Add a text button1
+
 button1 =  Button(root,  text="ASK" , bg="#356696" , pady=16 ,  padx=40,  borderwidth=3 , relief=SOLID ,  command=ask)
 button1.place(x= 70, y= 575)
 
-# Add a text button2
+
 button2 =  Button(root,  text="Send" , bg="#356696" , pady=16 ,  padx=40,  borderwidth=3 , relief=SOLID ,  command=User_send)
 button2.place(x= 400, y= 575)
 
-# Add a text button3
+
 button3 = Button(root, text="Delete", bg="#356696" , pady=16 ,  padx=40,  borderwidth=3 , relief=SOLID ,command=delete_text)
 button3.place(x= 225, y= 575)
 root.mainloop()
